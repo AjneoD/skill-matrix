2ab9b19f283e9014ec870870a3ba50cc2cc06ca8
XhoanaKasaj <xkasaj@unitsystem.net>
2022-07-05 10:58:29 +0000

    insert data

--- hasura-migrations/seeds/default/dummy.sql
+++ hasura-migrations/seeds/default/dummy.sql
@@ -1,1 +1,33 @@
-SELECT NOW();
+INSERT INTO "public"."users" ( "name")
+SELECT 
+  CONCAT('person', "person") AS "name"
+  
+FROM generate_series(1, 100) AS "person"
+
+ON CONFLICT ON CONSTRAINT "users_pkey"
+DO UPDATE SET 
+"name" = EXCLUDED."name"
+;
+
+
+INSERT INTO "public"."boards" ("name")
+SELECT 
+  CONCAT('person', "person") AS "name"
+  
+FROM generate_series(1, 100) AS "person"
+
+ON CONFLICT ON CONSTRAINT "boards_pkey"
+DO UPDATE SET 
+  "name" = EXCLUDED."name"
+  ;
+
+
+
+INSERT INTO "public"."boards_admins" ("board_id", "user_id")
+
+SELECT 
+  ("board") AS "board_id",
+  (floor(random() * (10 - 1 + 1) + 1)) AS "user_id"
+  
+FROM generate_series(1, 100) AS "board"
+;
